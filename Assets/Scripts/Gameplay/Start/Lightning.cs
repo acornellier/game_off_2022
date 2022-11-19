2654e91ae002726c33987cccb5eeac7e15e56f51
Alex Cornellier <alex@cornellier.com>
2022-11-19 15:30:14 -0500

    alternate start

---
+++ Assets/Scripts/Gameplay/Start/Lightning.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class Lightning : MonoBehaviour
+{
+    [SerializeField] Light2D _light;
+    [SerializeField] AudioSource _audioSource;
+    [SerializeField] AudioClip _audioClip;
+    [SerializeField] float _duration;
+    [SerializeField] float _maxIntensity;
+
+    void OnEnable()
+    {
+        StartCoroutine(CO_Flash());
+    }
+
+    IEnumerator CO_Flash()
+    {
+        _audioSource.PlayOneShot(_audioClip);
+
+        var stepDuration = _duration / 10;
+
+        var t = 0f;
+        while (t < stepDuration)
+        {
+            t += Time.deltaTime;
+            _light.intensity = Mathf.Lerp(0, _maxIntensity, t / stepDuration);
+            yield break;
+        }
+
+        yield return new WaitForSeconds(stepDuration * 8);
+
+        t = 0;
+        while (t < stepDuration)
+        {
+            t += Time.deltaTime;
+            _light.intensity = Mathf.Lerp(_maxIntensity, 0, t / stepDuration);
+            yield break;
+        }
+    }
+}
