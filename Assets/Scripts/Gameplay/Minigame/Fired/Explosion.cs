30c3c2f2b3349bed56aaf66b62c8c5e50bea67c0
Alex Cornellier <alex@cornellier.com>
2022-11-19 20:54:06 -0500

    fired explosion sound

--- Assets/Scripts/Gameplay/Minigame/Fired/Explosion.cs
+++ Assets/Scripts/Gameplay/Minigame/Fired/Explosion.cs
@@ -2,17 +2,33 @@
 using UnityEngine;
 
 [RequireComponent(typeof(CircleCollider2D))]
+[RequireComponent(typeof(SpriteRenderer))]
 public class Explosion : MonoBehaviour
 {
     [SerializeField] float _explosionTime = 0.3f;
 
+    CircleCollider2D _collider;
+    SpriteRenderer _renderer;
+
     bool _exploded;
-    float _explosionStartTime;
+    float _awakeTime;
     readonly List<Target> _alreadyHit = new();
 
     void Awake()
     {
-        Utilities.DestroyGameObject(gameObject, _explosionTime);
+        _collider = GetComponent<CircleCollider2D>();
+        _renderer = GetComponent<SpriteRenderer>();
+        _awakeTime = Time.time;
+        Utilities.DestroyGameObject(gameObject, _explosionTime * 4);
+    }
+
+    void Update()
+    {
+        if (Time.time - _awakeTime > _explosionTime)
+        {
+            _collider.enabled = false;
+            _renderer.enabled = false;
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
