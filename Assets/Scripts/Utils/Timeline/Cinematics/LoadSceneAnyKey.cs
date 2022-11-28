469cebc62be908ce6259ab8420a1a8f5a4dff6f5
Alex Cornellier <alex@cornellier.com>
2022-11-28 10:46:42 -0500

    remove dialogue from end cinematic

---
+++ Assets/Scripts/Utils/Timeline/Cinematics/LoadSceneAnyKey.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using Zenject;
+
+public class LoadSceneAnyKey : MonoBehaviour
+{
+    [SerializeField] string _scene;
+
+    [Inject] SceneLoader _sceneLoader;
+
+    bool _activated;
+
+    void Update()
+    {
+        if (_activated) return;
+
+        if (Keyboard.current.anyKey.isPressed || Mouse.current.leftButton.isPressed)
+        {
+            _activated = true;
+            _sceneLoader.SaveAndLoadScene(_scene);
+        }
+    }
+}
